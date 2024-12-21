c89cd28ad8362b7c225e050e9902529056fd84dc
Ajang Rahmat <41977490+ajangrahmat@users.noreply.github.com>
2024-12-21 10:31:31 +0700

    Update Example_With_Interval_Time_For_Sync

--- examples/Example_With_Interval_Time_For_Sync/Example_With_Interval_Time_For_Sync.ino
+++ examples/Example_With_Interval_Time_For_Sync/Example_With_Interval_Time_For_Sync.ino
@@ -1,6 +1,6 @@
 #include "KelasRobotTime.h"
-const char* ssid = "Kelas Robot";
-const char* pass = "kumaha aa we";
+const char* ssid = "WiFi Name";
+const char* pass = "Password Name";
 KelasRobotTime ntp;
 
 unsigned const long interval = 1000;
@@ -24,11 +24,13 @@ void loop() {
     Serial.print(":");
     Serial.println(ntp.seconds);
     Serial.println("Timer: " + String(timer));
+
     if (timer >= 3600) {
       timer = 0;
       ntp.connectWiFi(ssid, pass);
       ntp.syncTime();
       WiFi.mode(WIFI_OFF);
     }
+
   }
 }
